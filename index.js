0137e43aa6af6b13511a8b4aa61e0799a385a3cc
rosekamau <kamaurose236@gmail.com>
2024-07-08 23:42:47 +0300

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names,eventName){
+    const message = []
+    for (let i=0;i<names.length; i++){
+      message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+function countDown(number) {
+   let i =number
+   while(i >=0){
+    console.log(i);
+    i--;
+   }
+}
+
